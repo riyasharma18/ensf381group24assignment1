4f684967384160f0794c34405f24d54e6540a541
riyasharma18 <riya.sharma1@ucalgary.ca>
2024-03-23 15:01:29 -0600

    app.js

--- my-ecommerce-app/src/App.js
+++ my-ecommerce-app/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
+import Homepage from './components/Homepage';
+import ProductsPage from './components/ProductsPage';
+import LoginPage from './components/LoginPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/products" component={ProductsPage} />
+        <Route path="/login" component={LoginPage} />
+      </Switch>
+    </Router>
   );
 }
 
