0bb4b1780313ff25b3c3cfbeb0ba62c84c46bcdb
riyasharma18 <riya.sharma1@ucalgary.ca>
2024-03-29 14:49:10 -0600

    made Registration API

--- my-ecommerce-app/Backend/app.py
+++ my-ecommerce-app/Backend/app.py
@@ -3,3 +3,19 @@ import json
 import os
 
 app = Flask(__name__)
+
+users = []
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    information = request.json
+    username = information.get('username')
+    password = information.get('password')
+    email = information.get('email')
+    if any(user['username'] == username for user in users):
+        return jsonify({'error': 'This username already exists'})
+    users.append({'username': username, 'password': password, 'email': email})
+    return jsonify({'message': 'This user has been successfully registered'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
