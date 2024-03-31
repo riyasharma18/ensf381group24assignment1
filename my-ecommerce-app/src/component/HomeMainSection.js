b005eb219dd5d489af03e7f1223d819b928640d8
riyasharma18 <riya.sharma1@ucalgary.ca>
2024-03-30 20:03:08 -0600

    changed ProductList.js

--- my-ecommerce-app/src/component/HomeMainSection.js
+++ my-ecommerce-app/src/component/HomeMainSection.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
-import customerReviews from '../data/reviews'; 
+import customerReviews from '../data/products'; 
 
 function HomeMainSection() {
     const [randomReviews, setRandomReviews] = useState([]);
--- my-ecommerce-app/src/component/ProductList.js
+++ my-ecommerce-app/src/component/ProductList.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProductItem from './ProductItem';
-import product from '../data/products';
 
-function ProductList({addToCart}) {
+function ProductList({ addToCart }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('/products') // Assuming the React app is served from the same origin as the Flask server
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error('Error fetching products:', error));
+  }, []);
+
   return (
     <div className="product-list">
-      {product.map(product => (
+      {products.map(product => (
         <ProductItem key={product.id} product={product} addToCart={addToCart} />
       ))}
     </div>
